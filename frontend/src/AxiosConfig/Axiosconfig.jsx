14f26f158bbe1e9d4587dc89a9e7ae61703378b4
Anila Benny <135572616+AnilaBenny@users.noreply.github.com>
2024-12-26 01:27:09 +0530

    Update Axiosconfig.jsx

--- frontend/src/AxiosConfig/Axiosconfig.jsx
+++ frontend/src/AxiosConfig/Axiosconfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://employeeManagement.mooo.com",
+  baseURL: "https://employeeManage.mooo.com",
   timeout: 100000,
   headers: {
     'Content-Type': 'application/json',
